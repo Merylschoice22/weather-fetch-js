9acd02150ea14775713690f5d0898695fa5ff0cd
merylschoice22 <kekrieg.digital@gmail.com>
2021-05-21 01:03:02 +0200

    Enable button function when both fields have inputs

--- App.js
+++ App.js
@@ -5,45 +5,43 @@ h2.innerText = "Input coordinates to locate a city";
 h3.innerText = "The city temperature will be shown here";
 document.body.appendChild(h2);
 document.body.appendChild(h3);
+let lat = document.getElementById("lat");
+let lon = document.getElementById("lon");
 
-/*** VARIABLES ***/
-// let lat = "";
-// let lon = "";
-// console.log("BEFORE lat " + lat, " lon " + lon);
+/*** FORM VALIDATION ***/
+const getBtn = document.querySelector("#getBtn");
+getBtn.disabled = true;
+
+////// BUTTON DISABLED //////
+
+const enableButton = () => {
+  if (lat.value !== "" && lon.value !== "") {
+    getBtn.disabled = false;
+  }
+};
+
+lat.addEventListener("change", enableButton);
+lon.addEventListener("change", enableButton);
+
+////// INPUT TO NUMBER //////
+const isNumber = (lat, lon) => {
+  return parseFloat(lat) && parseFloat(lon);
+};
+
 /*** INPUT FETCH ***/
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
-
-  let lat = document.getElementById("lat").value;
-  let lon = document.getElementById("lon").value;
-  console.log("IN THE FORM lat " + lat, " lon " + lon);
-  (isValid(lat, lon) &&
-    fetch(`https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lon}`)
+  (isNumber(lat.value, lon.value) &&
+    fetch(
+      `https://fcc-weather-api.glitch.me/api/current?lat=${lat.value}&lon=${lon.value}`
+    )
       .then((response) => response.json())
       .then((temperatures) => {
-        console.log("Coordinates THEN: " + lat, lon);
         h3.innerText = `The current temperature there is ${temperatures.main.temp} degrees Celsius`;
         h2.innerText = `You have input coordinates for ${temperatures.name.toUpperCase()}`;
         document.body.appendChild(h2);
         document.body.appendChild(h3);
       })
       .catch((error) => console.log(`Error: ${error}`))) ||
-    alert("Invalid inputs!");
+    alert("Please enter valid coordinates in both fields");
 });
-
-/*** FORM VALIDATION***/
-
-// const validateForm = (lat, lon) => {
-//   console.log("VALIDATION INPUTS lat " + lat, " lon " + lon);
-
-//   if (lat == "" || lon == "") {
-//     alert("One or more of your inputs are empty!");
-//   }
-
-// };
-
-const isValid = (lat, lon) => {
-  console.log("VALIDATION INPUTS lat " + lat, " lon " + lon);
-
-  return parseFloat(lat) && parseFloat(lon);
-};
